1641f2aa8bb1a135781b46fe173b58c31a0eb299
NathanRodet <rodetnathan.contact@gmail.com>
2023-05-22 10:19:52 +0200

    Added gateway subgraphs

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      playground: true,
-      autoSchemaFile: true,
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'users', url: 'http://localhost:3001/graphql' },
+            { name: 'grades', url: 'http://localhost:3002/graphql' },
+            { name: 'classes', url: 'http://localhost:3003/graphql' },
+          ],
+        }),
+      },
     }),
   ],
   controllers: [AppController],
